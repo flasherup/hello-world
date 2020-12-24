01c6d96a1e251767f72dd298227d3fa6bb2060bb
Valeriy Vostrikov <>
2020-12-24 19:07:28 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { Container, Row, Col, ListGroup, Button} from 'react-bootstrap';
+import AddForm from './components/AddForm';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            songs:[]
+        }
+    }
+    render() {
+        return (
+            <Container>
+                <Row>
+                    <Col>
+                        <h1>Play List</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <AddForm onAddSong={values=>this.onAddSong(values)}/>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <h2>My songs list:</h2>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <ListGroup>
+                            {this.listFromArray(this.state.songs)}
+                        </ListGroup>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
+
+    listFromArray(songs) {
+        const listItems = songs.map((song, index)=>(
+            <ListGroup.Item key={`${ song.artist }${ song.song }`}>
+                <Row>
+                    <Col>
+                        {`${ index + 1 }: ${ song.artist }, ${ song.song }`}
+                    </Col>
+                    <Col>
+                        <Button variant='outline-danger' onClick={()=>this.onDelete(index)}>Delete</Button>
+                    </Col>
+                </Row>
+            </ListGroup.Item>
+        ));
+        console.log(listItems);
+        return listItems;
+    }
+
+    onAddSong(values) {
+        this.setState({songs:[...this.state.songs, values]});
+    }
+
+    onDelete(index) {
+        const filtered = this.state.songs.filter((_, i) => {
+            return i !== index;
+        })
+        this.setState({songs:[...filtered]});
+    }
+}
+
+export default App;
---
+++ src/components/AddForm.js
@@ -0,0 +1,58 @@
+import React,{Component} from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+const FIELD_ID_ARTIST = 'artist';
+const FIELD_ID_SONG = 'song';
+
+export default class AddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+           artist:'',
+           song:'' 
+        };
+    }
+    render() {
+        return(
+            <Form>
+                <Form.Group controlId={ FIELD_ID_ARTIST } >
+                    <Form.Label>Artist</Form.Label>
+                    <Form.Control  
+                        onChange={event=>this.onFormChange(event)}
+                        value={this.state.artist} 
+                    />
+                </Form.Group>
+                <Form.Group controlId={ FIELD_ID_SONG } >
+                    <Form.Label>Song</Form.Label>
+                    <Form.Control  
+                        onChange={event=>this.onFormChange(event)}
+                        value={this.state.song}   
+                    />
+                </Form.Group>
+                <Form.Group >
+                    <Button onClick={()=>this.onSubmit()}>Add</Button>
+                </Form.Group>
+            </Form>
+        );
+    }
+
+    onSubmit() {
+        this.props.onAddSong({...this.state});
+        this.setState({
+            artist:'',
+            song:'' 
+         });
+    }
+
+    onFormChange(event) {
+        switch(event.target.id) {
+            case FIELD_ID_ARTIST: 
+                this.setState({artist:event.target.value});
+            break;
+            case FIELD_ID_SONG:
+                this.setState({song:event.target.value});
+            break;
+            default:
+        }
+    }
+}
