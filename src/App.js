e223ae8e2c705dc79954d20fb1675e409fabb6df
qq <antoha.grom228@gmail.com>
2020-12-28 20:35:40 +0200

    My test commit!

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, ListGroup, Button} from 'react-bootstrap';
+import { Container, Row, Col, ListGroup, Button, Form} from 'react-bootstrap';
 import AddForm from './components/AddForm';
-import {main} from './components/practice'
 
 class App extends Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            songs:[]
+            songs:[],
+            editable:{index: -1, song:'', artist:''}
         }
-        main();
     }
     render() {
         return (
@@ -42,18 +41,47 @@ class App extends Component {
     }
 
     listFromArray(songs) {
-        const listItems = songs.map((song, index)=>(
-            <ListGroup.Item key={`${ song.artist }${ song.song }`}>
+        const listItems = songs.map((song, index)=>{
+            console.log(this.state.editable.index);
+            if (index === this.state.editable.index){
+            return <ListGroup.Item key={`${ song.artist }${ song.song }`}>
+            <Row>
+                <Col>
+                    <Form>
+                        <Row>
+                            <Col>
+                                <Form.Control 
+                                onChange={event=>this.onChangeArtist(event)}
+                                value={song.artist} 
+                                />
+                            </Col>
+                            <Col>
+                                <Form.Control 
+                                onChange={event=>this.onChangeSong(event)}
+                                value={song.song}
+                                />
+                            </Col>
+                        </Row>
+                    </Form>
+                </Col>
+                <Col>
+                    <Button variant='outline-danger' onClick={()=>this.onSave(index)}>Save</Button>
+                </Col>
+            </Row>
+        </ListGroup.Item>
+        }
+            return <ListGroup.Item key={`${ song.artist }${ song.song }`}>
                 <Row>
                     <Col>
                         {`${ index + 1 }: ${ song.artist }, ${ song.song }`}
                     </Col>
                     <Col>
+                        <Button variant='outline-danger' onClick={()=>this.onEdit(index)}>Edit</Button>
                         <Button variant='outline-danger' onClick={()=>this.onDelete(index)}>Delete</Button>
                     </Col>
                 </Row>
             </ListGroup.Item>
-        ));
+        });
         return listItems;
     }
 
@@ -61,11 +89,33 @@ class App extends Component {
         this.setState({songs:[...this.state.songs, values]});
     }
 
+    onChangeSong(event){
+        this.setState({song: event.target.value});
+    }
+  
+    onChangeArtist(event) {
+        this.setState({artist: event.target.value});
+    }
+
+    onEdit(index){
+        this.setState({editable:{index, artist:'', song:''}});
+    }
+    
+
     onDelete(index) {
         const filtered = this.state.songs.filter((_, i) => {
             return i !== index;
         })
         this.setState({songs:[...filtered]});
+    }
+
+    onSave(values) {
+        console.log(values);
+        this.props.onAddSong({...this.state});
+        this.setState({
+            artist:'',
+            song:'' 
+         });
     }
 }
 
