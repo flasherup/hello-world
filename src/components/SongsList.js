e834f905de8c3a5b5b0a285cdaea346b31dd57e5
Valeriy Vostrikov <>
2021-01-14 20:47:57 +0200

    Songs list fixes

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -3,31 +3,29 @@ import {Row, Col, Button, ListGroup} from 'react-bootstrap';
 
 export default class SongsList  extends Component {
     render() {
-        console.log( "Songs", this.props )
+        const items = this.listFromArray(this.props.songs);
         return (
         <ListGroup>
-            {this.listFromArray(this.props.songs)
-            }
+            { items }
         </ListGroup>
       )
     }
 
-    onDelete(index) {
-        this.props.onDelete(index)
-    }
-
     listFromArray(songs) {
-        const listItems = songs.map((song, index)=>
-        (<ListGroup.Item key={`songKey${ index }`}>
-                <Row>
-                    <Col>
-                        {`${ index + 1 }: ${ song.artist }, ${ song.song }`}
-                    </Col>
-                    <Col>
-                        <Button variant='outline-danger' onClick={()=>this.onDelete(index)}>Delete</Button>
-                    </Col>
-                </Row>
-            </ListGroup.Item>));
+        const listItems = songs.map((song, index)=>{
+            return (
+                <ListGroup.Item key={`songKey${ index }`}>
+                    <Row>
+                        <Col>
+                            {`${ index + 1 }: ${ song.artist }, ${ song.song }`}
+                        </Col>
+                        <Col>
+                            <Button variant='outline-danger' onClick={()=>this.props.onDelete(index)}>Delete</Button>
+                        </Col>
+                    </Row>
+                </ListGroup.Item>
+            )
+        });
         return listItems;
     }
 }
