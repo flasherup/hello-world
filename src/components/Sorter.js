1361cf573758c87497cde679be455f7a0823b553
Valeriy Vostrikov <>
2021-01-18 20:44:07 +0200

    Fix artist sorting function

--- src/components/Sorter.js
+++ src/components/Sorter.js
@@ -19,15 +19,22 @@ export default class Sorter extends Component {
 
     sortByArtist() {
         const sorted = [...this.props.songs];
-        sorted.sort((a,b)=>a.artist - b.artist);
+        sorted.sort((a,b)=>{
+            const artistA = a.artist.toUpperCase();
+            const artistB = b.artist.toUpperCase();
+            return artistA<artistB?-1:1;
+        });
+        console.log('sorted', sorted);
     }
 
     sortBySong() {
         const sorted = [...this.props.songs];
         sorted.sort((a,b)=>a.song - b.song);
+        console.log('sorted', sorted);
     }
 
     sortByDefault() {
         const sorted = [...this.props.songs];
+        console.log('sorted', sorted);
     }
 }
